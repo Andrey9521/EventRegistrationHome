0965b1c2eef3a81d75e93c843af7bcd6cd4d88e3
Andrey Andrey <a.baranetz2011@gmail.com>
2026-02-10 20:55:34 +0200

    Initial commit

---
+++ EventRegistration/Controllers/EventConroller.cs
@@ -0,0 +1,32 @@
+﻿using EventRegistration.Data;
+using EventRegistration.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventRegistration.Controllers;
+
+public class EventController : Controller
+{
+    // GET
+    public IActionResult Register(int id)
+    {
+        var eventItem = DataStore.Events.FirstOrDefault(e => e.Id == id);
+        if(eventItem == null) {return NotFound("Подію не найдено");}
+        
+        ViewBag.EventId = eventItem.Id;
+        ViewBag.EventTitle = eventItem.Title;
+        ViewBag.EventDescription = eventItem.Description;
+        ViewBag.EventDate = eventItem.EventDate.ToString("dd.MM.yyyy HH:mm");
+        ViewBag.EventLocation = eventItem.Location;
+        
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Register(Registration registration)
+    {
+        registration.Id = DataStore.GetNextRegistrationId();
+        registration.RegisteredAt = DateTime.Now;
+        DataStore.Registrations.Add(registration);
+        return View("Success");
+    }
+}
---
+++ EventRegistration/Data/DataStore.cs
@@ -0,0 +1,25 @@
+﻿using EventRegistration.Models;
+
+namespace EventRegistration.Data;
+
+public class DataStore
+{
+    public static List<Event> Events { get; set; } = new List<Event>();
+    
+    public static List<Registration> Registrations { get; set; } = new List<Registration>();
+
+    private static int _eventCounter = 1;
+    private static int _registrationCounter = 1;
+
+    public static int GetNextEventId()
+    {
+        return _eventCounter++;
+    }
+
+    public static int GetNextRegistrationId()
+    {
+        return _registrationCounter++;
+    }
+    
+}
+
